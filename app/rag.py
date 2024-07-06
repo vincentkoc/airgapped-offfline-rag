9894e66591a332f7862886bc5822c061593d2c82
Vincent Koc <vincentkoc@ieee.org>
2024-07-06 16:22:46 +1000

    Update rag.py

--- app/rag.py
+++ app/rag.py
@@ -1,11 +1,18 @@
 from langchain.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceEmbeddings
+from fastembed import TextEmbedding
 from utils import load_config
 
 config = load_config()
 
+def get_embedding_function():
+    if config['use_fast_embed']:
+        return TextEmbedding()
+    else:
+        return HuggingFaceEmbeddings(model_name=config['embedding_model'])
+
 def retrieve_context(query, top_k=3):
-    embeddings = HuggingFaceEmbeddings(model_name=config['embedding_model'])
+    embeddings = get_embedding_function()
     vectorstore = Chroma(persist_directory="./chroma_db", embedding_function=embeddings)
 
     docs = vectorstore.similarity_search(query, k=top_k)

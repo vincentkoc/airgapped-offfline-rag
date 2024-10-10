79107da15192307971760a4402b5529738337aa9
Vincent Koc <vincentkoc@ieee.org>
2024-10-10 14:29:28 +1100

    feat: licence

--- app/main.py
+++ app/main.py
@@ -1,3 +1,20 @@
+# This file is part of airgapped-offline-rag.
+#
+# Airgapped Offline RAG is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Airgapped Offline RAG is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Airgapped Offline RAG. If not, see <https://www.gnu.org/licenses/>.
+#
+# Copyright (C) 2024 Vincent Koc (https://github.com/vincentkoc)
+
 import streamlit as st
 import sys
 import os
